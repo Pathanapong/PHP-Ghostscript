526441aee42443a5ff6255cacc678fdbba76582c
AMEC (IS) Pathanapong Sokpukeaw <patanapo@MitsubishiElevatorAsia.co.th>
2024-12-02 09:40:50 +0700

    Initial commit

--- demo.php
+++ demo.php
@@ -1,7 +1,7 @@
 <?php
-	$program = "gswin32c.exe";
-	$sourceFile = 'source.pdf';
-	$sourcePass = 'Aa1234567890@Bb';
-	$outputFile = 'output.pdf';
-    system('"'.$program.'" -q -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dPreserveAnnots=false -dAutoRotatePages=/None -sFONTPATH="C:/WINDOWS/Fonts" -sOutputFile="'.$outputFile.'" -sPDFPassword="'.$sourcePass.'" -f "'.$sourceFile.'"');
+$program = "gswin32c.exe";
+$sourceFile = 'source.pdf';
+$sourcePass = 'Aa1234567890@Bb';
+$outputFile = 'output.pdf';
+system('"'.$program.'" -q -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dPreserveAnnots=false -dAutoRotatePages=/None -sFONTPATH="C:/WINDOWS/Fonts" -sOutputFile="'.$outputFile.'" -sPDFPassword="'.$sourcePass.'" -f "'.$sourceFile.'"');
 ?>
